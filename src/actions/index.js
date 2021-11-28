5d261be65abd0e4332b8bcbc7cc6c84cdef085d2
Victor oc <ochando3d@gmail.com>
2021-11-28 19:12:39 +0000

    CRUD action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,14 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+} from "./types";
+import streams from "../apis/streams";
+import { formValues } from "redux-form";
 
 export const signIn = (userId) => {
     return {
@@ -10,5 +20,39 @@ export const signIn = (userId) => {
 export const signOut = () => {
     return {
         type: SIGN_OUT,
+    };
+};
+
+export const createStream = (formValues) => {
+    return async (dispatch) => {
+        const response = await streams.post("/streams", formValues);
+        dispatch({ type: CREATE_STREAM, payload: response.data });
+    };
+};
+
+export const fetchStreams = () => {
+    return async (dispatch) => {
+        const response = await streams.get("/streams");
+        dispatch({ type: FETCH_STREAMS, payload: response.data });
+    };
+};
+export const fetchStream = (id) => {
+    return async (dispatch) => {
+        const response = await streams.get(`/streams/${id}`);
+        dispatch({ type: FETCH_STREAMS, payload: response.data });
+    };
+};
+
+export const editStream = (id, formValues) => {
+    return async (dispatch) => {
+        const response = await streams.put(`/streams/${id}`, formValues);
+        dispatch({ type: EDIT_STREAM, payload: response.data });
+    };
+};
+
+export const deleteStream = (id) => {
+    return async (dispatch) => {
+        await streams.delete(`/streams/${id}`);
+        dispatch({ type: DELETE_STREAM, payload: id });
     };
 };
