52c6198bdb6b5e5c3505cb5ebb9003b661fb1709
Victor oc <ochando3d@gmail.com>
2021-11-28 19:13:49 +0000

    streamCreate functionality

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm, touch } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions/index";
 
 class StreamCreate extends React.Component {
     renderError({ error, touched }) {
@@ -26,7 +28,9 @@ class StreamCreate extends React.Component {
         );
     };
 
-    onSubmit(formValues) {}
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return (
@@ -63,6 +67,8 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({ form: "streamCreate", validate: validate })(
+const formWrapped = reduxForm({ form: "streamCreate", validate: validate })(
     StreamCreate
 );
+
+export default connect(null, { createStream })(formWrapped);
